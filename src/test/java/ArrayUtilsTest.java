d7cdc1e6aa9563e74baabe087e644d0fa5836d56
char <yancheng.chu@sjsu.edu>
2025-03-31 17:46:38 -0400

    cicd

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -15,6 +15,22 @@ public class ArrayUtilsTest {
         int[] x2 = { 1, 2, 3, 2 };
         int result2 = ArrayUtils.findLast(x2, 2);
         assertEquals(3, result2);
+
+        int[] x3 = {};
+        int result3 = ArrayUtils.findLast(x3, 2);
+        assertEquals(-1, result3);
+
+        int[] x4 = { 2 };
+        int result4 = ArrayUtils.findLast(x4, 2);
+        assertEquals(-1, result4);
+
+        int[] x5 = { 3, 2 };
+        int result5 = ArrayUtils.findLast(x5, 2);
+        assertEquals(1, result5);
+
+        int[] x6 = { 2, 3 };
+        int result6 = ArrayUtils.findLast(x6, 2);
+        assertEquals(-1, result6);
     }
 
     @Test
@@ -26,6 +42,18 @@ public class ArrayUtilsTest {
         int[] x2 = { 1, 2, 3 };
         int result2 = ArrayUtils.lastZero(x2);
         assertEquals(-1, result2);
+
+        int[] x3 = {};
+        int result3 = ArrayUtils.lastZero(x3);
+        assertEquals(-1, result3);
+
+        int[] x4 = { 0 };
+        int result4 = ArrayUtils.lastZero(x4);
+        assertEquals(0, result4);
+
+        int[] x5 = { 1, 0 };
+        int result5 = ArrayUtils.lastZero(x5);
+        assertEquals(1, result5);
     }
 
     @Test
@@ -37,6 +65,18 @@ public class ArrayUtilsTest {
         int[] x2 = { -1, -2, -3 };
         int result2 = ArrayUtils.countPositive(x2);
         assertEquals(0, result2);
+
+        int[] x3 = {};
+        int result3 = ArrayUtils.countPositive(x3);
+        assertEquals(0, result3);
+
+        int[] x4 = { 0 };
+        int result4 = ArrayUtils.countPositive(x4);
+        assertEquals(1, result4);
+
+        int[] x5 = { 1, 2, 3 };
+        int result5 = ArrayUtils.countPositive(x5);
+        assertEquals(3, result5);
     }
 
     @Test
@@ -52,6 +92,18 @@ public class ArrayUtilsTest {
         int[] x3 = { 2, 4, 6 };
         int result3 = ArrayUtils.oddOrPos(x3);
         assertEquals(3, result3);
+
+        int[] x4 = {};
+        int result4 = ArrayUtils.oddOrPos(x4);
+        assertEquals(0, result4);
+
+        int[] x5 = { 1, 3, 5 };
+        int result5 = ArrayUtils.oddOrPos(x5);
+        assertEquals(3, result5);
+
+        int[] x6 = { -1, -3, -5 };
+        int result6 = ArrayUtils.oddOrPos(x6);
+        assertEquals(0, result6);
     }
 
     @Test
@@ -68,6 +120,30 @@ public class ArrayUtilsTest {
         int[] nums3 = {};
         int result3 = ArrayUtils.findLength(nums3, 5);
         assertEquals(0, result3);
+
+        int[] nums4 = { 10 };
+        int result4 = ArrayUtils.findLength(nums4, 5);
+        assertEquals(0, result4);
+
+        int[] nums5 = { 6, 7, 8 };
+        int result5 = ArrayUtils.findLength(nums5, 5);
+        assertEquals(0, result5);
+
+        int[] nums6 = { 1, 2, 3, 4, 1, 1 };
+        int result6 = ArrayUtils.findLength(nums6, 5);
+        assertEquals(1, result6);
+
+        int[] nums7 = { 3, 1, 2, 1 };
+        int result7 = ArrayUtils.findLength(nums7, 4);
+        assertEquals(2, result7);
+
+        int[] nums8 = { 4 };
+        int result8 = ArrayUtils.findLength(nums8, 4);
+        assertEquals(0, result8);
+
+        int[] nums9 = { 1, 2, 1, 2 };
+        int result9 = ArrayUtils.findLength(nums9, 3);
+        assertEquals(1, result9);
     }
 
     @Test
@@ -83,5 +159,14 @@ public class ArrayUtilsTest {
 
         int[] multiTarget = { 5, 2, 5, 4, 5 };
         assertEquals(3, ArrayUtils.countOf(multiTarget, 5));
+
+        int[] allTarget = { 5, 5, 5 };
+        assertEquals(3, ArrayUtils.countOf(allTarget, 5));
+
+        int[] singleElement = { 5 };
+        assertEquals(1, ArrayUtils.countOf(singleElement, 5));
+
+        int[] singleNonTarget = { 1 };
+        assertEquals(0, ArrayUtils.countOf(singleNonTarget, 5));
     }
 }
---
+++ task3.md
@@ -0,0 +1,3 @@
+https://hub.docker.com/r/sasayaaoi/task3
+
+https://github.com/Yancheng-Chu/ci-helloworld/
